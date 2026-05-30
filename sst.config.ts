f86c7f88d300b3a59e0349e620ddeacb092257f5
longhorn396 <devindrawhorn@gmail.com>
2026-05-30 11:35:55 -0500

    Adding DynamoDB table for the recipe selection

--- sst.config.ts
+++ sst.config.ts
@@ -9,8 +9,17 @@ export default $config({
     }
   },
   async run() {
+    const recipeTable = new sst.aws.Dynamo('RecipeTable', {
+      fields: {
+        recipeName: 'string',
+        cook: 'string',
+        lunch: 'string',
+      },
+      primaryIndex: { hashKey: 'recipeName' },
+    })
     new sst.aws.Nextjs('Portfolio', {
       domain: $app.stage === 'production' ? 'drawhorn.com' : 'drawhorn.click',
+      link: [recipeTable],
     })
   },
 })
