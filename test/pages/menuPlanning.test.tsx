53819f851e81a13942ea11ebf63f520c3dc3aedb
longhorn396 <devindrawhorn@gmail.com>
2026-07-04 10:00:27 -0500

    Fixing menu planning tests

--- test/pages/menuPlanning.test.tsx
+++ test/pages/menuPlanning.test.tsx
@@ -3,8 +3,21 @@
  */
 import { render } from '@testing-library/react'
 import MenuPlanningPage from '../../pages/menuPlanning'
+import apiRequest from '../../utils/apiRequest'
+
+jest.mock('../../utils/apiRequest', () => ({
+  __esModule: true,
+  default: jest.fn(),
+}))
+
+const mockedApiRequest = apiRequest as jest.MockedFunction<typeof apiRequest>
+let menuPlanningPage: ReturnType<typeof render>
 
 beforeEach(() => {
+  mockedApiRequest.mockImplementation((_path, _params, callback) => {
+    callback({ json: async () => ({ recipes: [] }) })
+  })
+
   menuPlanningPage = render(<MenuPlanningPage />)
 })
 
