3052c10784242541f5e79aeac59797e4fac18f75
longhorn396 <devindrawhorn@gmail.com>
2026-07-04 10:07:30 -0500

    adding error test case for menu planning

--- test/pages/menuPlanning.test.tsx
+++ test/pages/menuPlanning.test.tsx
@@ -1,7 +1,8 @@
 /**
  * @jest-environment jsdom
  */
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
+import { act } from 'react'
 import MenuPlanningPage from '../../pages/menuPlanning'
 import apiRequest from '../../utils/apiRequest'
 
@@ -13,16 +14,41 @@ jest.mock('../../utils/apiRequest', () => ({
 const mockedApiRequest = apiRequest as jest.MockedFunction<typeof apiRequest>
 let menuPlanningPage: ReturnType<typeof render>
 
-beforeEach(() => {
+beforeEach(async () => {
+  jest.clearAllMocks()
+
   mockedApiRequest.mockImplementation((_path, _params, callback) => {
     callback({ json: async () => ({ recipes: [] }) })
   })
 
-  menuPlanningPage = render(<MenuPlanningPage />)
+  await act(async () => {
+    menuPlanningPage = render(<MenuPlanningPage />)
+    await Promise.resolve()
+  })
 })
 
 describe('Menu Planning page', () => {
   it('matches snapshot', () => {
     expect(menuPlanningPage).toMatchSnapshot()
+  })
+
+  it('handles the error path from apiRequest', async () => {
+    mockedApiRequest.mockImplementation((_path, _params, _callback, onError, fin) => {
+      onError?.()
+      fin?.()
+    })
+
+    await act(async () => {
+      render(<MenuPlanningPage />)
+    })
+
+    expect(screen.getAllByText('Menu Planning').length).toBeGreaterThan(0)
+    expect(mockedApiRequest).toHaveBeenCalledWith(
+      '/api/menuPlanning',
+      { method: 'GET' },
+      expect.any(Function),
+      expect.any(Function),
+      expect.any(Function),
+    )
   })
 })
