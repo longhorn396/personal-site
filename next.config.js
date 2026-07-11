29a1f85fd86fed9a1bdcd7493197b755c46c6208
Devin Drawhorn <longhorn396@users.noreply.github.com>
2026-07-10 21:15:40 -0500

    Enable unoptimized images in Next.js config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 export default {
+  images: {
+    unoptimized: true,
+  },
   output: 'standalone',
   reactStrictMode: true,
   transpilePackages: ['@mui/material', '@mui/material-nextjs', 'material-react-table'],
